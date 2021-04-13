35cc3ae0be723751e2690d6ec4a01f4f5f332d2d
Indira Dutta <66861242+indiradutta@users.noreply.github.com>
2021-04-13 20:57:40 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,28 +4,32 @@ from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-model=Sequential()
+def model():
 
-#Convolution blocks
-model.add(Conv2D(32, kernel_size = (3,3), padding='same',input_shape=(300,300,3),activation='relu'))
-model.add(MaxPooling2D(pool_size=2)) 
+  model=Sequential()
 
-model.add(Conv2D(64, kernel_size = (3,3), padding='same',activation='relu'))
-model.add(MaxPooling2D(pool_size=2)) 
+  #Convolution blocks
+  model.add(Conv2D(32, kernel_size = (3,3), padding='same',input_shape=(300,300,3),activation='relu'))
+  model.add(MaxPooling2D(pool_size=2)) 
 
-model.add(Conv2D(32, kernel_size = (3,3), padding='same',activation='relu'))
-model.add(MaxPooling2D(pool_size=2)) 
+  model.add(Conv2D(64, kernel_size = (3,3), padding='same',activation='relu'))
+  model.add(MaxPooling2D(pool_size=2)) 
 
-#Classification layers
-model.add(Flatten())
+  model.add(Conv2D(32, kernel_size = (3,3), padding='same',activation='relu'))
+  model.add(MaxPooling2D(pool_size=2)) 
 
-model.add(Dense(64,activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(32,activation='relu'))
+  #Classification layers
+  model.add(Flatten())
 
-model.add(Dropout(0.2))
-model.add(Dense(6,activation='softmax'))
+  model.add(Dense(64,activation='relu'))
+  model.add(Dropout(0.2))
+  model.add(Dense(32,activation='relu'))
 
-model.compile(optimizer = 'Adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+  model.add(Dropout(0.2))
+  model.add(Dense(6,activation='softmax'))
 
-model.fit_generator(train_datagen,epochs=4,steps_per_epoch=2753//32)
+  model.compile(optimizer = 'Adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+
+  model.fit_generator(train_datagen,epochs=4,steps_per_epoch=2753//32)
+  
+  return model
